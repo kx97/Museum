8ce289c4410a6269d8825cd9115120ded5041834
kx97 <1425236749@qq.com>
2019-03-04 18:39:36 +0800

    dist

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 
-
 document.ready(function(e) {
   var itemlist = document.querySelector('#items');
   show(items1, itemlist);
@@ -28,7 +27,7 @@ document.ready(function(e) {
         pitems[j].style.display = 'none';
       }
     }
-}
+  }
 });
 // 改变季数时改变显示内容
 function change(element, itemlist) {
@@ -55,60 +54,60 @@ function change(element, itemlist) {
   }
 }
 // 定义添加HTML的函数
-  function show(items, element) {
-  
-    for(var i = 0; i < items.length; i++) {
-      var str = '';
-      str += '<div class="item" id="item'+ i +'">';
-      str += ' <div class="item-left">';
-      str += ' <div class="title">';
-      str += '  <h3>' + items[i].Museum + ' | ' + items[i].name +'</h3>';
-      str += ' </div>';
-      str += ' <div class="pic">';
-      str += '  <img class="img-item" src ="./src/img/'+ items[i].name +'.jpg" alt="' + items[i].name +'">';
-      str += ' </div>';
-      str += ' </div>';
-      str += ' <div class="item-right">';
-      str += ' <div class="subtitle">' + items[i].subtitle + '</div>';
-      str += ' <div class="desc">' + items[i].description + '</div>';
-      str += ' </div>';
-      str += ' <div class="link">';
-      str += ' <a href="' + items[i].link + '" target="_blank">了解 ></a>';
-      str += ' </div>';
-      str += '</div>';
-      str += '<hr>';
-      element.innerHTML += str;
+function show(items, element) {
 
-      var pitems = document.querySelectorAll(".box .box-list .list li p");
-      pitems[i].innerHTML = items[i].name;
-    }  
-  }
- // 鼠标点击图片触发模态框事件
-  function modalClick() {
-    var modal = document.querySelector('.modal');
-    var modalClose = document.querySelector('.modal-close');
-    var modalCont = document.querySelector('.modal-content');
-    var modalCover = document.querySelector('.modal-cover');
-    var cloTop = modalClose.style.top;
-    var contTop = modalCont.style.top;
-    var coverTop = modalCover.style.top;
-    var Imgs = document.querySelectorAll('.pic');
-    for(let i = 0; i < Imgs.length; i++) {
-      Imgs[i].onclick = function(e) {
-        modal.style.display = 'block';
-        modalCont.src = e.target.src;
-        modalClose.style.top = window.pageYOffset + parseInt(cloTop) + 'px';
-        modalCont.style.top = window.pageYOffset + parseInt(contTop) + 'px';
-        modalCover.style.top = window.pageYOffset + parseInt(coverTop) + 'px';
-        document.body.style.overflow = 'hidden';
-      };
-    }
-    modalClose.onclick = function(e) {
-      modal.style.display = 'none';
-      modalCont.src = '';
-      modalClose.style.top = cloTop;
-      modalCont.style.top = contTop;
-      modalCover.style.top = coverTop;
-      document.body.style.overflow = 'visible';
+  for(var i = 0; i < items.length; i++) {
+    var str = '';
+    str += '<div class="item" id="item'+ i +'">';
+    str += ' <div class="item-left">';
+    str += ' <div class="title">';
+    str += '  <h3>' + items[i].Museum + ' | ' + items[i].name +'</h3>';
+    str += ' </div>';
+    str += ' <div class="pic">';
+    str += '  <img class="img-item" src ="./src/img/'+ items[i].name +'.jpg" alt="' + items[i].name +'">';
+    str += ' </div>';
+    str += ' </div>';
+    str += ' <div class="item-right">';
+    str += ' <div class="subtitle">' + items[i].subtitle + '</div>';
+    str += ' <div class="desc">' + items[i].description + '</div>';
+    str += ' </div>';
+    str += ' <div class="link">';
+    str += ' <a href="' + items[i].link + '" target="_blank">了解 ></a>';
+    str += ' </div>';
+    str += '</div>';
+    str += '<hr>';
+    element.innerHTML += str;
+
+    var pitems = document.querySelectorAll(".box .box-list .list li p");
+    pitems[i].innerHTML = items[i].name;
+  }  
+}
+// 鼠标点击图片触发模态框事件
+function modalClick() {
+  var modal = document.querySelector('.modal');
+  var modalClose = document.querySelector('.modal-close');
+  var modalCont = document.querySelector('.modal-content');
+  var modalCover = document.querySelector('.modal-cover');
+  var cloTop = modalClose.style.top;
+  var contTop = modalCont.style.top;
+  var coverTop = modalCover.style.top;
+  var Imgs = document.querySelectorAll('.pic');
+  for(let i = 0; i < Imgs.length; i++) {
+    Imgs[i].onclick = function(e) {
+      modal.style.display = 'block';
+      modalCont.src = e.target.src;
+      modalClose.style.top = window.pageYOffset + parseInt(cloTop) + 'px';
+      modalCont.style.top = window.pageYOffset + parseInt(contTop) + 'px';
+      modalCover.style.top = window.pageYOffset + parseInt(coverTop) + 'px';
+      document.body.style.overflow = 'hidden';
     };
   }
+  modalClose.onclick = function(e) {
+    modal.style.display = 'none';
+    modalCont.src = '';
+    modalClose.style.top = cloTop;
+    modalCont.style.top = contTop;
+    modalCover.style.top = coverTop;
+    document.body.style.overflow = 'visible';
+  };
+}
